abbd384be6af2e6773bc0cef491abe4775152c58
EdgarLuna <eluna@urchinsys.com>
2023-05-04 23:01:58 -0500

    Agregando dependencias al test

--- src/customer/customer.service.spec.ts
+++ src/customer/customer.service.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomerService } from './customer.service';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerEntity } from './customer.entity';
+import { IdentificationEntity } from './identification.entity';
+import { AddressEntity } from './address.entity';
 
 describe('CustomerService', () => {
   let service: CustomerService;
@@ -7,6 +12,17 @@ describe('CustomerService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CustomerService],
+      imports: [
+        TypeOrmModule.forRoot({
+          ...TypeOrmTestingConfig(),
+          entities: [CustomerEntity, IdentificationEntity, AddressEntity],
+        }),
+        TypeOrmModule.forFeature([
+          CustomerEntity,
+          IdentificationEntity,
+          AddressEntity,
+        ]),
+      ],
     }).compile();
 
     service = module.get<CustomerService>(CustomerService);
--- src/shared/testing-utils/typeorm-testing-config.ts
+++ src/shared/testing-utils/typeorm-testing-config.ts
@@ -1,20 +1,9 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { IdentificationEntity } from '../../customer/identification.entity';
-import { AddressEntity } from '../../customer/address.entity';
-import { CustomerEntity } from '../../customer/customer.entity';
-
-export const TypeOrmTestingConfig = () => [
-  TypeOrmModule.forRoot({
+export const TypeOrmTestingConfig = (): any => {
+  return {
     type: 'sqlite',
     database: ':memory:',
-    entities: [CustomerEntity, IdentificationEntity, AddressEntity],
     dropSchema: true,
     synchronize: true,
     keepConnectionAlive: true,
-  }),
-  TypeOrmModule.forFeature([
-    CustomerEntity,
-    IdentificationEntity,
-    AddressEntity,
-  ]),
-];
+  };
+};
